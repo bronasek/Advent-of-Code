0615e469abc42870d8a7934157c6040df713b9e4
Kamil Swider <kamil.b.swider@gmail.com>
2018-12-14 12:55:46 +0100

    added solutions

--- 2018/05/P1.js
+++ 2018/05/P1.js
@@ -1,9 +1,31 @@
-// 04 - Repose Record - Part One
+// 05 - Alchemical Reduction - Part One
 
 const fs = require('fs');
 
 const partOne = () => {
+   let inputValues = [];
+   inputValues = fs.readFileSync('./input.txt').toString().split("");
    
+   const checkCapital = (i, iPlus1) => {
+      if ( (/[a-z]/.test(i) && /[A-Z]/.test(iPlus1)) || (/[A-Z]/.test(i) && /[a-z]/.test(iPlus1)) ) {
+         return true;
+      }
+      return false;
+   }
+   
+   for (let i = 0; i < inputValues.length - 1; ) {
+      if(inputValues[i].toUpperCase() === inputValues[i + 1].toUpperCase()
+         && checkCapital(inputValues[i], inputValues[i + 1])) {
+         inputValues.splice(i, 2)
+         if (i > 0) {
+            i--;
+         }
+      } else {
+         i++;
+      }
+   }
+   
+   console.log(inputValues.length);
 }
 
 partOne();
--- 2018/05/P2.js
+++ 2018/05/P2.js
@@ -1,9 +1,44 @@
-// 04 - Repose Record - Part Two
+// 05 - Alchemical Reduction - Part Two
 
 const fs = require('fs');
 
-const partTwo = () => {
+let inputValues = [];
+inputValues = fs.readFileSync('./input.txt').toString().split("");
+
+const partTwoReduction = (input) => {
+   const checkCapital = (i, iPlus1) => {
+      if ( (/[a-z]/.test(i) && /[A-Z]/.test(iPlus1)) || (/[A-Z]/.test(i) && /[a-z]/.test(iPlus1)) ) {
+         return true;
+      }
+      return false;
+   }
    
+   for (let i = 0; i < input.length - 1; ) {
+      if(input[i].toUpperCase() === input[i + 1].toUpperCase()
+         && checkCapital(input[i], input[i + 1])) {
+         input.splice(i, 2);
+         if (i > 0) {
+            i--;
+         } 
+      } else {
+         i++;
+      }
+   }
+   
+   return input;
 }
 
-partTwo();
+const partTwoResult = () => {
+   let initialReduction = partTwoReduction(inputValues);
+   let result = initialReduction.length;
+   
+   for (let i = 65; i <= 90; i++) {
+      const reducedInput = initialReduction.filter( val => val.charCodeAt(val) !== i && val.charCodeAt(val) !== (i + 32) );
+      const doubleReduction = partTwoReduction(reducedInput).length;
+      doubleReduction < result ? result = doubleReduction : result;
+   }
+   
+   console.log(result);
+}
+
+partTwoResult();
